8c102fea027e2acfe00d74ec58e127dd0865d993
Parthiv <parthivshetty1@gmail.com>
2024-08-13 17:56:04 +0530

    updated polar plotter

--- prime_polar_plotter/prime_polar_plotter.py
+++ prime_polar_plotter/prime_polar_plotter.py
@@ -1,5 +1,9 @@
 import pygame
 import math
+
+
+scale = int(input("Enter scaling factor\nThe bigger the number the more you zoom out (200 is pretty good)"))# downscaling
+
 pygame.init()
 
 
@@ -9,15 +13,20 @@ screen = pygame.display.set_mode((width,height))
 running = True
 
 centrex,centrey = (width//2, height//2)
-
-
+white = (255,255,255)
+cyan = (112, 243, 255)
 font_size = 20
 font = pygame.font.SysFont(None, font_size)
 ls = []
-def plot(r,theta):
+def plot(r,theta,color):
     x = centrex + int(r * math.sin(theta))
     y = centrey + int(r* math.cos(theta))
-    pygame.draw.circle(screen,(255,255,255),(x,y),1)
+    pygame.draw.circle(screen,color,(x,y),1)
+    
+def plot_line(r,theta):
+    x = centrex + int(r * math.sin(theta))
+    y = centrey + int(r* math.cos(theta))
+    pygame.draw.line(screen,cyan,(centrex,centrey),(x,y),2)
 
 def ls_stuff(x):
     if x==1:
@@ -28,14 +37,13 @@ def ls_stuff(x):
     return True
 
 kk = 0
-scale = 1000 # downscaling
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((0,0,0))
-    plot(0,0)
+    plot(0,0,white)
     kk += 1
     if (ls_stuff(kk)):
         ls.append(kk)
@@ -44,8 +52,10 @@ while running:
     screen.blit(text, text_rect)
 
     for j in ls:
-        plot(j/scale,j)
+        plot(j/scale,j,white)
     pygame.display.flip()
+
+    plot(j/scale,j,cyan)
     
 
 pygame.quit()
